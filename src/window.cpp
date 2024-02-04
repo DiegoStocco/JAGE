db8f6c8f032943f52725f6eea068bad402bf55bd
DiegoStocco <diegostocco.2008@gmail.com>
2024-02-04 17:33:28 +0100

    removed hardcoded opengl versions

--- src/window.cpp
+++ src/window.cpp
@@ -6,10 +6,6 @@
 namespace JAGE {
   Window::Window(std::string name, int width, int height)
   {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
     // Create a windowed mode window and its OpenGL context
     m_window = glfwCreateWindow(width, height, name.c_str(), NULL, NULL);
     if (!m_window)
